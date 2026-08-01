50c1cdf0e6ed16885f0e6ceb10f0eab0f78ad293
alattinkarademir-sudo <alattinkarademir@gmail.com>
2026-08-01 18:40:31 +0300

    Fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,23 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
-    {
-      url: "https://www.tachmekanik.com.tr/about",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://www.tachmekanik.com.tr/services",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.9,
-    },
-    {
-      url: "https://www.tachmekanik.com.tr/contact",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
   ];
 }
