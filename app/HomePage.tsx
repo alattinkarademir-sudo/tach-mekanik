0346228107cddf7280ae9068ec8334cc4b3fc3b0
alattinkarademir-sudo <alattinkarademir@gmail.com>
2026-08-07 19:28:03 +0300

    Ana sayfa bölümleri ayrıldı

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -1,16 +1,9 @@
 import HeroSlider from "@/components/HeroSlider";
-import About from "@/components/About";
-import Services from "@/components/Services";
-import Contact from "@/components/Contact";
-
 
 export default function HomePage() {
   return (
     <main className="pt-20 md:pt-24">
       <HeroSlider />
-      <About />
-      <Services />
-      <Contact />
     </main>
   );
 }
